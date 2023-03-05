5943e40f73803aed63822ffb46d6a3bd4b648e1c
Toby Devlin <toby@tobydevlin.com>
2023-03-05 14:22:30 +1100

    try python log dir names + resolve

--- src/data_processing/const.py
+++ src/data_processing/const.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
-_data_root = Path(__file__).parent.parent.parent / "data"
+_data_root = (Path(__file__).parent.parent.parent / "data").resolve()
 crime_path = _data_root / "Crime_Data_from_2020_to_Present.csv"
 reddit_path = _data_root / "reddit_account_data.csv"
 out_path = _data_root / "output"
 
 if not out_path.exists():
     out_path.mkdir(parents=True)
+
+print(crime_path)
+print(reddit_path)
+print(out_path)
